241c16be1ecf98ee3407322c1d760bc63d44696e
Moritz Laass <moritz.laass@gmail.com>
2019-08-17 20:42:01 +0200

    ex7 centroid means completed

--- ex7/computeCentroids.m
+++ ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%COMPUTECENTROIDS returns the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -26,7 +26,11 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+for i = 1:K
+    mask = idx == i;
 
+    centroids(i, :) = (1/sum(mask)) .* sum(mask .* X);
+endfor
 
 
 
